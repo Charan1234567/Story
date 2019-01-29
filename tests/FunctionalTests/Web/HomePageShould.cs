e06feb762319e6b83f7d5612aa92e4eba2e6b2bf
Denise Case <dcase@nwmissouri.edu>
2019-01-29 09:42:19 -0600

    initial tests run

---
+++ tests/FunctionalTests/Web/HomePageShould.cs
@@ -0,0 +1,24 @@
+using Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalTests.Web
+{
+    public class HomePageShould : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+        public HomePageShould(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+        [Fact]
+        public async Task ReturnHttpStatusCodeOk()
+        {
+            var response = await _client.GetAsync("/");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
